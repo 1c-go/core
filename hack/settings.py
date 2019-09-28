417f3da035b15ea86e36fa4c923e811f618e076b
jlemyp <3541970tim>
2019-09-28 17:17:41 +0500

    nginx

--- hack/settings.py
+++ hack/settings.py
@@ -131,13 +131,13 @@ STATIC_ROOT = 'static'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '_storage/'
+MEDIA_ROOT = 'media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 # MEDIA_URL = 'http://127.0.0.1:8000/_storage/'
-MEDIA_URL = '/_storage/'
+MEDIA_URL = '/media/'
 
 # -------- rest --------
 
