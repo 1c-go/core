a8b71c9fd789810c511c85c74b8eb844c989eab6
jlemyp <3541970tim>
2019-09-29 01:02:24 +0500

    notifications

--- questionnaire/models/discussion.py
+++ questionnaire/models/discussion.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 
 from main.models.user import CustomUser
@@ -37,3 +38,31 @@ class Discussion(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        new = not self.id
+        super().save(force_insert, force_update, using, update_fields)
+
+        if new:
+            data = {
+                'notification': {
+                    'body': self.preview,
+                    'title': 'Новое обсуждение',
+                    'sound': 'default'
+                },
+                'priority': 'high',
+                'data': {
+                    'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+                    'id': '1',
+                    'status': 'done'
+                },
+                'to': 'cJB0a_cN1Us:APA91bFuZ47cosiH3NafguF7GdHySh_lbR5_B2LCs29Fh7tCVZSeip46LMiYuBiQGZo'
+                      'Dbx9fss4SwOwZxjh2tVcDtGZTnhlyBdzaqRRFfC4oEiowhe0_bhZfCyInPw_SRxKf_cX-T2G6'
+            }
+            response = requests.post('https://fcm.googleapis.com/fcm/send', json=data, headers={
+                'Authorization': 'key=AAAA5AXWsQU:APA91bH9WJLN4IDMMBb631TzHuQCPrmTMZyZ328mIEPWgedff'
+                                 'YZTjxRL5_WQ6g_vT5lbbHNBZDiCQCgG6ydAkH2fpsZW5zGp_IYKTNu5YUSOAngT'
+                                 'zoviK66LNGCATR1p3EeOcBac5E2n'
+            })
+            print(response.content)
