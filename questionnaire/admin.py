334b8a6e08289f0dcfb91de5522298d0fc5e588a
jlemyp <3541970tim>
2019-09-29 11:25:45 +0500

    update admin

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,9 +1,10 @@
-from django.contrib.admin import ModelAdmin, register, TabularInline
+from django.contrib.admin import register, TabularInline
 from django_q.admin import QueueAdmin, FailAdmin, TaskAdmin, ScheduleAdmin
 from django_q.conf import Conf
 from django_q.models import OrmQ, Schedule, Success, Failure
+from reversion.admin import VersionAdmin
 
-from main.admin import admin_site
+from main.admin import admin_site, full_admin_site
 from .models.answer import Answer
 from .models.answer_variant import AnswerVariant
 from .models.comment import Comment
@@ -13,16 +14,16 @@ from .models.news import News
 from .models.question import Question
 from .models.topic import Topic
 
-admin_site.register(Schedule, ScheduleAdmin)
-admin_site.register(Success, TaskAdmin)
-admin_site.register(Failure, FailAdmin)
+full_admin_site.register(Schedule, ScheduleAdmin)
+full_admin_site.register(Success, TaskAdmin)
+full_admin_site.register(Failure, FailAdmin)
 
 if Conf.ORM or Conf.TESTING:
-    admin_site.register(OrmQ, QueueAdmin)
+    full_admin_site.register(OrmQ, QueueAdmin)
 
 
 @register(News, site=admin_site)
-class NewsAdmin(ModelAdmin):
+class NewsAdmin(VersionAdmin):
     list_filter = ('id', 'title', 'created_at')
     list_display = ('id', 'title', 'created_at')
 
@@ -36,19 +37,19 @@ class DiscussionTable(TabularInline):
 
 
 @register(Topic, site=admin_site)
-class TopicAdmin(ModelAdmin):
+class TopicAdmin(VersionAdmin):
     list_display = ('id', 'name')
     search_fields = ('name',)
     inlines = (DiscussionTable,)
 
 
 @register(Comment, site=admin_site)
-class CommentAdmin(ModelAdmin):
+class CommentAdmin(VersionAdmin):
     list_display = ('id', 'discussion', 'user', 'text', 'sentiment')
 
 
 @register(Like, site=admin_site)
-class LikeAdmin(ModelAdmin):
+class LikeAdmin(VersionAdmin):
     list_display = ('id', 'discussion', 'user', 'value')
     search_fields = ('discussion__name', 'user__full_name')
     list_filter = ('value',)
@@ -79,7 +80,7 @@ class LikeTable(TabularInline):
 
 
 @register(Discussion, site=admin_site)
-class DiscussionAdmin(ModelAdmin):
+class DiscussionAdmin(VersionAdmin):
     list_display = ('id', 'topic', 'name', 'type')
     list_filter = ('type',)
     search_fields = ('topic__name', 'name')
@@ -102,14 +103,14 @@ class AnswerTable(TabularInline):
 
 
 @register(Question, site=admin_site)
-class QuestionAdmin(ModelAdmin):
+class QuestionAdmin(VersionAdmin):
     list_display = ('id', 'question', 'answer_type')
     search_fields = ('question',)
     inlines = (AnswerVariantTable, AnswerTable)
 
 
 @register(Answer, site=admin_site)
-class AnswerAdmin(ModelAdmin):
+class AnswerAdmin(VersionAdmin):
     list_display = ('id', 'question', 'user', 'answer', 'sentiment')
     list_filter = ('question', 'answer')
     search_fields = ('question__question',)
